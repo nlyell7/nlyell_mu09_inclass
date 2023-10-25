746507759b94f6bccfbc5c53090e68f9ad16fc8f
Nathan Lyell <nathanlyell@Nathans-MacBook-Pro.local>
2023-10-24 20:49:38 -0400

    New test commit

---
+++ new_package/my_mod.py
@@ -0,0 +1,2 @@
+def print_square():
+    print('Be there or be square')
---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setuptools
+setup(
+    name = "Demo",
+    version = '1.1',
+    description = 'This package does not do much',
+    url = 'https://github.com/nlyell7/nlyell_mu09_inclass.git',
+    author = 'Nathan Lyell',
+    author_email = 'nathan.r.lyell@gmail.com',
+    license = 'MIT',
+    packages = ['demo']
+
+)
